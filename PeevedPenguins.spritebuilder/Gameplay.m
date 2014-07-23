54d9e52f6155be49889a20a1921325e0185f939d
Ginny <ginspi@ymail.com>
2014-07-22 17:34:19 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -13,7 +13,8 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
-    
+    CCNode *_pullbackNode;
+
 }
 
 // is called when CCB file has completed loading
@@ -27,6 +28,9 @@
     
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
+    
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
 
 }
 
